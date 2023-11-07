ed3896a05fbabf237ff80bdbfc84c87912d005c8
Kate <z.katusha5@gmail.com>
2023-11-07 21:58:37 +0300

    добавила файлы программы и функции

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-
-
 #5.	Реализовать модуль, содержащий функцию нахождения в массиве целых чисел
 #   разности индексов максимального и минимального элементов.
 
 import find_difference
 
-n = int(input("Enter the size of the array: "))
+n = input("Enter the size of the array: ")
+while True:
+    try:
+        n = int(n)
+        break
+    except:
+        n = input('Enter correct size of the array:  ')
+
 arr = []
 for i in range(n):
-    arr.append(int(input()))
+    x = input('Enter element: ')
+    while True:
+        try:
+            x = int(x)
+            break
+        except:
+            x = input('Enter correct previous element:  ')
+    arr.append(x)
 
-dif = find_difference.difference(arr)
-print(dif)
+print(find_difference.difference(arr))
